78407b565ffe512e3363a0179300fc10a4983d22
Lukas Joergensen <lukaspj@outlook.com>
2017-09-08 10:59:05 +0200

    Fix for deployment on PaaS

--- src/app/shopping-list/shopping-list-routing.module.ts
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -3,17 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListsModule } from './lists/lists.module';
 import { ItemsModule } from './items/items.module';
 
+export function loadItemsModule() {
+  return ItemsModule;
+}
+export function loadListsModule() {
+  return ListsModule;
+}
+
 export const _shoppingListRoutes: Routes = [
   {
     path: 'shopping-list',
     children: [
       {
         path: 'items',
-        loadChildren: () => ItemsModule
+        loadChildren: loadItemsModule
       },
       {
         path: 'lists',
-        loadChildren: () => ListsModule
+        loadChildren: loadListsModule
       }
     ]
   }
