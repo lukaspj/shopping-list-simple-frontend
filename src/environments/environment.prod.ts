be7b85612a48be6562dad168decd25111b11419e
Lukas Joergensen <lukaspj@outlook.com>
2017-09-13 13:59:18 +0200

    Update the production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -20,10 +20,25 @@ export const environment = {
       'getAll': 'http://sample-db-service.flynn.lukasj.org/list_items/'
     },
     recipes: {
-      'create': 'http://sample-db-service.flynn.lukasj.org/recipes/create'
+      'create': 'http://sample-db-service.flynn.lukasj.org/recipes/create',
+      'list': 'http://sample-db-service.flynn.lukasj.org/recipes',
+      'get': (id) => `http://sample-db-service.flynn.lukasj.org/recipes/${id}`,
+      'delete': 'http://sample-db-service.flynn.lukasj.org/recipes/delete',
+      'update': 'http://sample-db-service.flynn.lukasj.org/recipes/update'
     },
     ingredients: {
-
+      'list': 'http://sample-db-service.flynn.lukasj.org/ingredients',
+      'get': (id) => `http://sample-db-service.flynn.lukasj.org/ingredients/${id}`,
+      'create': `http://sample-db-service.flynn.lukasj.org/ingredients/create`,
+      'update': `http://sample-db-service.flynn.lukasj.org/ingredients/update`,
+      'delete': `http://sample-db-service.flynn.lukasj.org/ingredients/delete`
+    },
+    recipe_ingredients: {
+      'list': 'http://sample-db-service.flynn.lukasj.org/recipe_ingredients',
+      'list_for': (id) => `http://sample-db-service.flynn.lukasj.org/recipe_ingredients/${id}`,
+      'create': 'http://sample-db-service.flynn.lukasj.org/recipe_ingredients/create',
+      'update': 'http://sample-db-service.flynn.lukasj.org/recipe_ingredients/update',
+      'delete': 'http://sample-db-service.flynn.lukasj.org/recipe_ingredients/delete'
     }
   }
 };
