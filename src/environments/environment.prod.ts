53ecf2e245dbd3ee6139a49633cdc806ce0fee5f
Lukas Joergensen <lukaspj@outlook.com>
2017-09-13 16:43:15 +0200

    Updated environments, and added different production db url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,44 +1,47 @@
+const dbUrl = 'https://normad-db.flynn.lukasj.org';
+
 export const environment = {
   production: true,
   serviceUrls: {
     items: {
-      'create': 'http://sample-db-service.flynn.lukasj.org/items/create',
-      'delete': 'http://sample-db-service.flynn.lukasj.org/items/delete',
-      'getAll': 'http://sample-db-service.flynn.lukasj.org/items'
+      create: `${dbUrl}/items/create`,
+      delete: `${dbUrl}/items/delete`,
+      getAll: `${dbUrl}/items`
     },
     lists: {
-      'create': 'http://sample-db-service.flynn.lukasj.org/lists/create',
-      'delete': 'http://sample-db-service.flynn.lukasj.org/lists/delete',
-      'update': 'http://sample-db-service.flynn.lukasj.org/lists/update',
-      'get': 'http://sample-db-service.flynn.lukasj.org/lists/',
-      'getAll': 'http://sample-db-service.flynn.lukasj.org/lists/',
+      create: `${dbUrl}/lists/create`,
+      delete: `${dbUrl}/lists/delete`,
+      update: `${dbUrl}/lists/update`,
+      get: `${dbUrl}/lists/`,
+      getAll: `${dbUrl}/lists/`,
     },
     listed_item: {
-      'create': 'http://sample-db-service.flynn.lukasj.org/list_items/create',
-      'delete': 'http://sample-db-service.flynn.lukasj.org/list_items/delete',
-      'get': (id) => `http://sample-db-service.flynn.lukasj.org/list_items/${id}`,
-      'getAll': 'http://sample-db-service.flynn.lukasj.org/list_items/'
+      create: `${dbUrl}/list_items/create`,
+      delete: `${dbUrl}/list_items/delete`,
+      get: (id) => `${dbUrl}/list_items/${id}`,
+      getAll: `${dbUrl}/list_items/`
     },
     recipes: {
-      'create': 'http://sample-db-service.flynn.lukasj.org/recipes/create',
-      'list': 'http://sample-db-service.flynn.lukasj.org/recipes',
-      'get': (id) => `http://sample-db-service.flynn.lukasj.org/recipes/${id}`,
-      'delete': 'http://sample-db-service.flynn.lukasj.org/recipes/delete',
-      'update': 'http://sample-db-service.flynn.lukasj.org/recipes/update'
+      create: `${dbUrl}/recipes/create`,
+      list: `${dbUrl}/recipes`,
+      latest: (limit) => `${dbUrl}/recipes/latest/${limit}`,
+      get: (id) => `${dbUrl}/recipes/${id}`,
+      delete: `${dbUrl}/recipes/delete`,
+      update: `${dbUrl}/recipes/update`
     },
     ingredients: {
-      'list': 'http://sample-db-service.flynn.lukasj.org/ingredients',
-      'get': (id) => `http://sample-db-service.flynn.lukasj.org/ingredients/${id}`,
-      'create': `http://sample-db-service.flynn.lukasj.org/ingredients/create`,
-      'update': `http://sample-db-service.flynn.lukasj.org/ingredients/update`,
-      'delete': `http://sample-db-service.flynn.lukasj.org/ingredients/delete`
+      list: `${dbUrl}/ingredients`,
+      get: (id) => `${dbUrl}/ingredients/${id}`,
+      create: `${dbUrl}/ingredients/create`,
+      update: `${dbUrl}/ingredients/update`,
+      delete: `${dbUrl}/ingredients/delete`
     },
     recipe_ingredients: {
-      'list': 'http://sample-db-service.flynn.lukasj.org/recipe_ingredients',
-      'list_for': (id) => `http://sample-db-service.flynn.lukasj.org/recipe_ingredients/${id}`,
-      'create': 'http://sample-db-service.flynn.lukasj.org/recipe_ingredients/create',
-      'update': 'http://sample-db-service.flynn.lukasj.org/recipe_ingredients/update',
-      'delete': 'http://sample-db-service.flynn.lukasj.org/recipe_ingredients/delete'
+      list: `${dbUrl}/recipe_ingredients`,
+      list_for: (id) => `${dbUrl}/recipe_ingredients/${id}`,
+      create: `${dbUrl}/recipe_ingredients/create`,
+      update: `${dbUrl}/recipe_ingredients/update`,
+      delete: `${dbUrl}/recipe_ingredients/delete`
     }
   }
 };
